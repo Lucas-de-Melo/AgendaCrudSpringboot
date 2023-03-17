d2353638ad830cf33ef8f943bd2dfedc3bf3b331
Lucas de Melo <lucas-pereira83@hotmail.com>
2023-03-17 16:33:58 -0300

    Criado atributos do contato

---
+++ agenda/src/main/java/by/lucas/agenda/contatos/modelo/contato.java
@@ -0,0 +1,25 @@
+package by.lucas.agenda.contatos.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class contato {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(nullable = false)
+  private String nome;
+
+  @Column(nullable = false)
+  private String telefone;
+
+  @Column(nullable = false)
+  private String email;
+
+  private String urlavatar;
+}
